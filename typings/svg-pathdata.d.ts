966ff17d695c15c3e51ef945f730a033dd2a029f
oxyflour <oxyflour@hotmail.com>
2016-01-02 23:17:59 +0800

    add typing

---
+++ typings/svg-pathdata.d.ts
@@ -0,0 +1,50 @@
+declare module 'svg-pathdata' {
+    class SVGPathData {
+        constructor(data: string)
+        commands: SVGPathData.Command[]
+        encode(): string
+    }
+
+    module SVGPathData {
+        export const CLOSE_PATH
+        export const MOVE_TO
+        export const HORIZ_LINE_TO
+        export const VERT_LINE_TO
+        export const LINE_TO
+        export const CURVE_TO
+        export const SMOOTH_CURVE_TO
+        export const QUAD_TO
+        export const SMOOTH_QUAD_TO
+        export const ARC
+        export const DRAWING_COMMANDS
+
+        export interface Command {
+            type: string
+        }
+
+        interface CommandCommon {
+            x: number
+            y: number
+            relative: boolean
+        }
+
+        export interface CommandMoveTo extends CommandCommon {
+        }
+
+        export interface CommandLineTo extends CommandCommon {
+        }
+
+        export interface CommandCurveTo extends CommandCommon {
+            x1: number
+            y1: number
+            x2: number
+            y2: number
+        }
+
+        export interface CommandClosePath { }
+
+        // tobe added
+    }
+
+    export = SVGPathData
+}
